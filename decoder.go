ed81caa3346a466cd335b2f7d8afc8e1b962857d
Andrey Semochkin <admin@deepweb.spb.ru>
2021-05-15 01:34:41 +0300

    debug

--- decoder.go
+++ decoder.go
@@ -16,7 +16,7 @@ type Decoder struct {
 	pkt   *avcodec.Packet
 }
 
-func NewDecoder() (decoder *Decoder, err error) {
+func NewDecoder(resize, crop, gpu string) (decoder *Decoder, err error) {
 	codec := &avcodec.Codec{}
 	ctx := &avcodec.Context{}
 	codec = avcodec.AvcodecFindDecoderByName("h264_cuvid")
@@ -25,9 +25,22 @@ func NewDecoder() (decoder *Decoder, err error) {
 		if codec == nil {
 			err = errors.New("AvcodecFindDecoderByName, Unsupported codec!")
 			return
+		} else {
+			//		log.Println("============================================================Open Codec decoder soft===============================================================")
 		}
+	} else {
+
 	}
 	ctx = codec.AvcodecAllocContext3()
+	if crop != "" {
+		ctx.SetEncodeParams("crop", crop, 0)
+	}
+	if resize != "" {
+		ctx.SetEncodeParams("resize", resize, 0)
+	}
+	if gpu != "" {
+		ctx.SetEncodeParams("gpu", gpu, 0)
+	}
 	if ctx.AvcodecOpen2(codec, nil) < 0 {
 		err = errors.New("AvcodecOpen2, open error")
 		return
@@ -36,26 +49,37 @@ func NewDecoder() (decoder *Decoder, err error) {
 	decoder = &Decoder{codec: codec, ctx: ctx, pkt: packet}
 	return
 }
-func (self *Decoder) Decode(data []byte) (img *image.YCbCr, err error) {
+func (self *Decoder) Decode(data []byte) (videoFrame *avutil.Frame, err error) {
 	var frameFinished int
-	var videoFrame *avutil.Frame
 	self.pkt.AvPacketFromByteSlice(data)
 	videoFrame = avutil.AvFrameAlloc()
 	self.ctx.AvcodecDecodeVideo2((*avcodec.Frame)(unsafe.Pointer(videoFrame)), &frameFinished, self.pkt)
 	if frameFinished > 0 {
-		img, err = avutil.GetPicture(videoFrame)
 		if err != nil {
 			err = errors.New("Image Error")
 		}
 	} else {
 		err = errors.New("No Image Frame")
 	}
-	avutil.AvFrameFree(videoFrame)
+
 	self.pkt.AvPacketUnref()
 	return
 }
+func (self *Decoder) GetPicturev5(name string, videoFrame *avutil.Frame) (img *image.YCbCr, err error) {
+	img, err = avutil.GetPicturev5(videoFrame)
+	return
+}
+func (self *Decoder) GetPicture(videoFrame *avutil.Frame, buffer *[]float32) (buf *[]float32, img *image.YCbCr, LastFrameFloat *float32, err error) {
+	buf, img, LastFrameFloat, err = avutil.GetPicture(videoFrame, buffer)
+
+	return
+}
+func (self *Decoder) GetPictureGray(videoFrame *avutil.Frame, buffer *[]float32) (buf *[]float32, img *image.YCbCr, LastFrameFloat *float32, err error) {
+	buf, img, LastFrameFloat, err = avutil.GetPictureGray(videoFrame, buffer)
+
+	return
+}
 func (self *Decoder) Close() {
-	avcodec.AvPacketFree(self.pkt)
 	avcodec.AvcodecFreeContext(self.ctx)
 	self.ctx.AvcodecClose()
 }
--- encoder.go
+++ encoder.go
@@ -16,32 +16,56 @@ type Encoder struct {
 	pkt   *avcodec.Packet
 }
 
-func NewEncoder() (encoder *Encoder, err error) {
+func NewEncoder(w, h, fps, bitrate int, gpu string) (encoder *Encoder, err error) {
 	codec := &avcodec.Codec{}
 	ctx := &avcodec.Context{}
-	codec = avcodec.AvcodecFindEncoderByName("h264_cuvid")
+	codec = avcodec.AvcodecFindEncoderByName("nvenc_h264")
 	if codec == nil {
 		codec = avcodec.AvcodecFindEncoder(avcodec.CodecId(avcodec.AV_CODEC_ID_H264))
 		if codec == nil {
 			err = errors.New("AvcodecFindEncoderByName, Unsupported codec!")
 			return
+		} else {
+			//	log.Println("============================================================Open Codec encoder nvenc_h264===============================================================")
 		}
+	} else {
+		//log.Println("============================================================Open Codec encoder nvenc_h264===============================================================")
 	}
 	ctx = codec.AvcodecAllocContext3()
-	ctx.SetBitRate(0)
-	ctx.SetWidth(1920)
-	ctx.SetHeight(1080)
-	ctx.SetTimeBase(avutil.NewRational(1, 25))
-	//ctx.SetFramerate(avutil.NewRational(25, 1))
-	ctx.SetGopSize(10)
-	//ctx.SetMaxBFrames(1)
-	ctx.SetEncodeParams("preset", "ultrafast", 0)
-	ctx.SetEncodeParams("profile", "baseline", 0)
-	//ctx.
+	//	ctx.SetBitRate(10000000) //// size of gop * 8 == bit_rate
+	//log.Fatalln(fps)
+	ctx.SetWidth(w)
+	ctx.SetHeight(h)
+	if gpu != "" {
+		ctx.SetEncodeParams("gpu", gpu, 0)
+	}
+	ctx.SetTimeBase(avutil.NewRational(1, fps))
+	ctx.SetGopSize(fps)
 	ctx.SetPixFmt(avcodec.AV_PIX_FMT_YUV420P)
 	if ctx.AvcodecOpen2(codec, nil) < 0 {
-		err = errors.New("AvcodecOpen2, open error")
-		return
+		//log.Println("============================================================Open Codec encoder ERROR===============================================================")
+		avcodec.AvcodecFreeContext(ctx)
+		codec = avcodec.AvcodecFindEncoderByName("libx264")
+		if codec != nil {
+			//log.Println("============================================================SWITCH Codec encoder libx264===============================================================")
+			ctx = codec.AvcodecAllocContext3()
+			ctx.SetBitRate(0)
+			ctx.SetWidth(w)
+			ctx.SetHeight(h)
+			ctx.SetTimeBase(avutil.NewRational(1, 1))
+			ctx.SetGopSize(1)
+			ctx.SetEncodeParams("preset", "ultrafast", 0)
+			ctx.SetEncodeParams("profile", "baseline", 0)
+			ctx.SetThreadCount(20)
+			ctx.SetPixFmt(avcodec.AV_PIX_FMT_YUV420P)
+			if ctx.AvcodecOpen2(codec, nil) < 0 {
+				err = errors.New("AvcodecOpen2, open error")
+				return
+			}
+		} else {
+			err = errors.New("AvcodecOpen2, open error")
+			return
+		}
 	}
 	encoder = &Encoder{codec: codec, ctx: ctx}
 	return
@@ -52,8 +76,6 @@ func (self *Encoder) Encode(img *image.YCbCr) (payload []byte, err error) {
 	packet := avcodec.AvPacketAlloc()
 	videoFrame = avutil.AvFrameAlloc()
 	avutil.SetPicture(videoFrame, img)
-	videoFrame.SetHeight(1080)
-	videoFrame.SetWidth(1920)
 	videoFrame.SetFormat(avcodec.AV_PIX_FMT_YUV420P)
 	self.ctx.AvcodecEncodeVideo2(packet, (*avcodec.Frame)(unsafe.Pointer(videoFrame)), &frameFinished)
 	if frameFinished > 0 {
@@ -66,6 +88,8 @@ func (self *Encoder) Encode(img *image.YCbCr) (payload []byte, err error) {
 	return
 }
 func (self *Encoder) Close() {
+	//log.Println("============================================================!!!!!!!!!!!!!!!!!!!!!!!!!!!! Close Codecs !!!!!!!!!!!!!!!!===============================================================")
 	avcodec.AvcodecFreeContext(self.ctx)
 	self.ctx.AvcodecClose()
+	//avcodec.Codec = nul
 }
